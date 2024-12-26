6e2b6c1e9a8d08f6fc34d37f232e6555ea91012a
Maria <mariabrodowska89@gmail.com>
2024-12-26 12:36:58 +0100

    register page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -28,13 +28,13 @@
         <!-- Remember Me -->
         <div class="flex items-center justify-between mt-2">
         <div class="block">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="mb-6 rounded bg-white border-gray-300 border-b-white text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800" name="remember">
-                <span class="ms-2 text-sm text-white">{{ __('Zapamiętaj mnie') }}</span>
+            <label for="remember_me" class="inline-flex items-center cursor-pointer hover:text-white/70">
+                <input id="remember_me" type="checkbox" class="mb-6 rounded bg-white border-gray-300 border-b-white text-pink-300 shadow-sm focus:ring-pink-300 dark:focus:ring-pink-300 dark:focus:ring-offset-gray-800 cursor-pointer" name="remember">
+                <span class="ms-2 text-sm text-white hover:text-white/70">{{ __('Zapamiętaj mnie') }}</span>
             </label>
         </div>
         @if (Route::has('password.request'))
-            <a class="text-sm text-white hover:text-white/70 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
+            <a class="text-sm text-white hover:text-white/70 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
                 {{ __('Zapomniałeś hasła?') }}
             </a>
         @endif
@@ -43,7 +43,7 @@
             {{ __('Zaloguj się') }}
         </x-primary-button>
         <span class="ms-2 font-thin text-sm text-white">Nie masz konta?</span>
-        <a class="text-sm font-bold text-white hover:text-white/70 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
+        <a class="text-sm font-bold text-white hover:text-white/70 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('register') }}">
             {{ __('Zarejestruj się') }}
         </a>
     </form>
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -15,7 +15,7 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans text-gray-900 antialiased">
-    <div class="h-screen w-full bg-cover animate-moveBackground" style="background-image: url({{ asset('assets/img/user3.png') }});">
+    <div class="h-screen w-full bg-cover animate-moveBackground" style="background-image: url({{ asset('assets/img/user6.png') }});">
         <div class="h-screen w-full bg-cover animate-moveBackground2" style="background-image: url({{ asset('assets/img/user22.png') }}); background-size: 800px 500px;">
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0">
 {{--            <div>--}}
